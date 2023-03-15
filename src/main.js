63ccbf40cc1b3ca8d2fa3f35148eb2fc943b027f
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-03-15 11:33:11 -0500

    Trending movies

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const API_TRENDING_PREVIEW = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+const API_CATEGORIES = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+
+async function fetchData(urlApi, API) {
+  const response = await fetch(urlApi, API);
+  const data = await response.json();
+  return data;
+}
+
+async function getTrendingMoviesPreview(urlApi) {
+  try {
+    const trendingPreviewMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
+    const res = await fetchData(urlApi);
+
+    const movies = res.results;
+    movies.forEach((movie) => {
+      const movieContainer = document.createElement("div");
+      movieContainer.classList.add("movie-container");
+
+      const movieImg = document.createElement("img");
+      movieImg.classList.add("movie-img");
+      movieImg.setAttribute("alt", movie.title);
+      movieImg.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+      );
+
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getTrendingMoviesPreview(API_TRENDING_PREVIEW);
